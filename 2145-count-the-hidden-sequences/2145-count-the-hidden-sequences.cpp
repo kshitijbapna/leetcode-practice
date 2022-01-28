b04daeef4bc942c572ccf740ca20e1b875cf1c95
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-01-28 23:38:33 +0530

    Time: 355 ms (19.40%), Space: 106.7 MB (28.47%) - LeetHub

--- 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    int numberOfArrays(vector<int>& d, int lower, int upper) {
+   int numberOfArrays(vector<int>& d, int lower, int upper) {
         int l=lower,r=upper,n=d.size(),i;
         long long int sum=0;
-        // vector<long long int> pre(n);
-        // pre[0]=d[0];
-        // for(int i=1;i<n;i++)pre[i]=pre[i-1]+d[i];
         for(i=0;i<n;i++){
             sum+=d[i];
             long long int ll,rr;

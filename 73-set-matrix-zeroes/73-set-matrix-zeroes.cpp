c56849077db84195d17b16fa8da7dd304e41d831
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-02-01 21:00:39 +0530

    Time: 12 ms (89.06%), Space: 13.2 MB (81.95%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool x[matrix.size()][matrix[0].size()];
-        memset(x,0,sizeof(x));
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j]==0&&x[i][j]==0){
-                    for(int k=0;k<matrix.size();k++){
-                        if(matrix[k][j]!=0)x[k][j]=1;
-                        matrix[k][j]=0;
-                    }
-                    for(int k=0;k<matrix[i].size();k++){
-                        if(matrix[i][k]!=0)x[i][k]=1;
-                        matrix[i][k]=0;
-                    }
+        int rows=matrix.size(),cols=matrix[0].size(),i,j;
+        bool r=0,c=0;
+        for(int i=0;i<cols;i++)if(matrix[0][i]==0)r=1;
+        for(int i=0;i<rows;i++)if(matrix[i][0]==0)c=1;
+
+        // if(matrix[0][0]==0)f=1;
+        for(int i=1;i<rows;i++){
+            for(int j=1;j<cols;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        // return matrix;
+        for(int i=1;i<rows;i++){
+            if(matrix[i][0]==0)
+            for(j=0;j<cols;j++)matrix[i][j]=0;
+        }
+        for(int i=1;i<cols;i++){
+            if(matrix[0][i]==0)
+            for(int j=0;j<rows;j++)matrix[j][i]=0;
+        }
+        if(r)for(int i=0;i<cols;i++)matrix[0][i]=0;
+        if(c)for(int i=0;i<rows;i++)matrix[i][0]=0;
     }
 };

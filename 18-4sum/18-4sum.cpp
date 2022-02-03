59dc6100dfd51474eee18cd2cd26b17f497a9b80
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-02-03 22:37:24 +0530

    Time: 51 ms (80.73%), Space: 13.1 MB (91.17%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,24 +1,54 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        set<vector<int>> s;
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-3;i++){
-            for(int j=i+1;j<n-2;j++){
-                for(int k=j+1;k<n-1;k++){
-                    long long int x=(long long int)target-nums[i]-nums[j]-nums[k];
-                    int in=upper_bound(nums.begin(),nums.end(),x)-nums.begin();
-                    in--;
-                    if(in<n&&in>k&&nums[in]==x){
-                        vector<int> y{nums[i],nums[j],nums[k],(int)x};
-                        if(s.count(y)==0)ans.push_back(y);
-                        s.insert(y);
+    vector<vector<int>> fourSum(vector<int>& num, int target) {
+        int n=num.size();
+        vector<vector<int>> res;
+        sort(num.begin(),num.end());
+         for (int i = 0; i < n; i++) {
+        
+            int target_3 = target - num[i];
+        
+            for (int j = i + 1; j < n; j++) {
+            
+                int target_2 = target_3 - num[j];
+            
+                int front = j + 1;
+                int back = n - 1;
+            
+                while(front < back) {
+                
+                    int two_sum = num[front] + num[back];
+                
+                    if (two_sum < target_2) front++;
+                
+                    else if (two_sum > target_2) back--;
+                
+                    else {
+                    
+                        vector<int> quadruplet(4, 0);
+                        quadruplet[0] = num[i];
+                        quadruplet[1] = num[j];
+                        quadruplet[2] = num[front];
+                        quadruplet[3] = num[back];
+                        res.push_back(quadruplet);
+                    
+                        // Processing the duplicates of number 3
+                        while (front < back && num[front] == quadruplet[2]) ++front;
+                    
+                        // Processing the duplicates of number 4
+                        while (front < back && num[back] == quadruplet[3]) --back;
+                
                     }
                 }
+                
+                // Processing the duplicates of number 2
+                while(j + 1 < n && num[j + 1] == num[j]) ++j;
             }
+        
+            // Processing the duplicates of number 1
+            while (i + 1 < n && num[i + 1] == num[i]) ++i;
+        
         }
-        return ans;
+        return res;
     }
 };

bc2700a8481a2b3da6095429aa1370c148a6ee46
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-02-03 22:13:41 +0530

    Time: 968 ms (6.00%), Space: 78.7 MB (7.06%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -5,9 +5,6 @@ public:
         set<vector<int>> s;
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        // cout<<upper_bound(nums.begin(),nums.end(),2)-nums.begin()<<"\n";
-        // for(int i=0;i<n;i++)cout<<nums[i]<<" ";
-        // cout<<"\n";
         for(int i=0;i<n-3;i++){
             for(int j=i+1;j<n-2;j++){
                 for(int k=j+1;k<n-1;k++){
@@ -15,7 +12,6 @@ public:
                     int in=upper_bound(nums.begin(),nums.end(),x)-nums.begin();
                     in--;
                     if(in<n&&in>k&&nums[in]==x){
-                        // cout<<"here";
                         vector<int> y{nums[i],nums[j],nums[k],(int)x};
                         if(s.count(y)==0)ans.push_back(y);
                         s.insert(y);

77af560e180ad0d934116b1cd4f4ebccc790d241
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-02-04 21:07:36 +0530

    Time: 7 ms (97.66%), Space: 7.9 MB (81.60%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -15,7 +15,7 @@ public:
         while(slow!=fast&&slow&&fast&&fast->next){
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast)return 1;
+            // if(slow==fast)return 1;
         }
         if(slow==fast)return 1;
         return 0;

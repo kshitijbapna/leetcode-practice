256e274d0d8e1c758cb146b6445855be2aa0becd
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-03-25 22:23:26 +0530

    Time: 12 ms (23.24%), Space: 10.1 MB (12.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    static bool cmp(vector<int> a,vector<int> b){
+        return a[1]-a[0]<b[1]-b[0];
+    }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        vector<int> refund;
         int N = costs.size()/2;
         int minCost = 0;
         for(vector<int> cost : costs){
             minCost += cost[0];
-            refund.push_back(cost[1] - cost[0]); 
         }
-        sort(refund.begin(), refund.end());
+        sort(costs.begin(), costs.end(),cmp);
         for(int i = 0;i<N; i++){
-            minCost += refund[i];
+            minCost += costs[i][1]-costs[i][0];
         }
         return minCost;
     }

23515c4c0d7d64781a97b47638a0b735eb154fac
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-03-25 23:07:28 +0530

    Time: 228 ms (81.89%), Space: 62.5 MB (71.02%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -4,7 +4,7 @@ auto it=[](pair<int,int> a,pair<int,int> b){
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& p, int k) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(it)>pq(it);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(it)> pq(it);
         for(int i=0;i<p.size();i++){
             int dis=p[i][0]*p[i][0]+p[i][1]*p[i][1];
             if(pq.size()==k){

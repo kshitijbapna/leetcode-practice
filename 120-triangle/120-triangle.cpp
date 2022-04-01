ef537b7c225677549e6467426c3da397f1213de6
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-04-01 19:44:22 +0530

    Time: 25 ms (5.35%), Space: 8.8 MB (48.99%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[205][205];
+    int fun(int i,vector<vector<int>>& tr,int n){
+        if(n<0||i<0)return 0;
+        if(dp[i][n]!=-1e9)return dp[i][n];
+        if(i==0){
+            return dp[i][n]=tr[n][i]+fun(0,tr,n-1);
+        }
+        if(i==tr[n].size()){
+            return dp[i][n]=tr[n][tr[n].size()-1]+fun(tr[n].size()-1,tr,n-1);
+        }
+        return dp[i][n]=tr[n][i]+min(fun(i-1,tr,n-1),fun(i,tr,n-1));
+    }
+    int minimumTotal(vector<vector<int>>& tr) {
+        for(int i=0;i<205;i++)for(int j=0;j<205;j++)dp[i][j]=-1e9;
+        int n=tr.size(),ans=INT_MAX;
+        for(int i=0;i<tr[n-1].size();i++){
+            ans=min(ans,fun(i,tr,n-1));
+        }
+        return ans;
+    }
+};

870cbb7e97177c9d7bd8a0858c2db9322fef30f4
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-04-13 00:30:20 +0530

    Time: 3 ms (44.52%), Space: 6 MB (37.21%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int minOperations(int n) {
-        // 1 3 5 7 9       
-        // 1 3 5 7 9 11
-        // 1 3 5 7 9 11 13 
-        int i=1,ans=0;
-        while(i<n){
-            ans+=n-i;
-            i+=2;
-        }
-        return ans;
+        return ((n+1)/2)*(n-(n+1)/2);
     }
 };

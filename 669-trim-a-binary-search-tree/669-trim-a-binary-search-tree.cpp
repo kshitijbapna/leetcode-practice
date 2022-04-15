409cd68649b7af0fa03b2fc1e37abe0b7cbb4b1b
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-04-15 11:59:50 +0530

    Time: 16 ms (80.86%), Space: 23.9 MB (44.36%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -11,42 +11,12 @@
  */
 class Solution {
 public:
-    void help(TreeNode* root,TreeNode*prev,int low,int high){
-        if(root==NULL)return;
-        if(root->val<low){
-            prev->left=root->right;
-            help(prev->left,prev,low,high);
-            return;
-        }
-        else if(root->val>high){
-            prev->right=root->left;
-            help(prev->right,prev,low,high);
-            return;
-        }
-        help(root->left,root,low,high);
-        help(root->right,root,low,high);
-    }
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-        int c=10;
-        int xx=root->val;
-        while(1){
-            while(root!=NULL&&root->val<low){
-                root=root->right;
-                // c++;
-            }
-            while(root!=NULL&&root->val>high){
-                root=root->left;
-                // c++;
-            }
-            if(root==NULL)break;
-            if(xx==root->val)break;
-            xx=root->val;
-            // if(c==0)break;
-        }
         if(root==NULL)return NULL;
-
-        help(root->left,root,low,high);
-        help(root->right,root,low,high);
+        if(root->val<low)return trimBST(root->right,low,high);
+        if(root->val>high)return trimBST(root->left,low,high);
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
         return root;
     }
 };

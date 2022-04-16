b766d461e3f4f5a56fe73be56a1426b5bf734448
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-04-16 13:11:45 +0530

    Time: 36 ms (90.34%), Space: 33.4 MB (62.62%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -14,22 +14,23 @@ public:
     int sum=0;
     void inorder1(TreeNode* root){
         if(root==NULL)return;
-        inorder1(root->left);
-        sum+=root->val;
         inorder1(root->right);
+        root->val+=sum;
+        sum=root->val;
+        inorder1(root->left);
     }
-    void inorder2(TreeNode* root){
-        if(root==NULL)return;
-        inorder2(root->left);
-        int tmp=root->val;
-        root->val=sum;
-        sum=root->val-tmp;
-        inorder2(root->right);
-    }
+    // void inorder2(TreeNode* root){
+    //     if(root==NULL)return;
+    //     inorder2(root->left);
+    //     int tmp=root->val;
+    //     root->val=sum;
+    //     sum=root->val-tmp;
+    //     inorder2(root->right);
+    // }
     TreeNode* convertBST(TreeNode* root) {
         inorder1(root);
         // cout<<sum;
-        inorder2(root);
+        // inorder2(root);
         return root;
     }
 };

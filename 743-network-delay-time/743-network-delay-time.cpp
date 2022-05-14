07d936e1b6777ce56196d7df11d517b78c49052f
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-05-14 13:04:51 +0530

    Time: 266 ms (15.90%), Space: 36.6 MB (98.34%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    int networkDelayTime(vector<vector<int>>& t, int n, int k) {
+        int dis[n+1];
+        for(int i=0;i<=n;i++)dis[i]=INT_MAX;
+        dis[k]=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<t.size();j++){
+                if(dis[t[j][0]]!=INT_MAX&&dis[t[j][0]]+t[j][2]<dis[t[j][1]])
+                    dis[t[j][1]]=dis[t[j][0]]+t[j][2];
+            }
+        }
+        int ans=0;
+        for(int i=1;i<=n;i++)if(dis[i]==INT_MAX)return -1;
+        else ans=max(ans,dis[i]);
+        return ans;
+    }
+};

60be317838a30a58e807a06011fe33c9e78ea933
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-05-25 14:29:22 +0530

    Time: 456 ms (62.42%), Space: 80.1 MB (35.74%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -4,36 +4,19 @@ public:
         if(a[0]==b[0])return a[1]>b[1];
         return a[0]<b[0];
     }
-    int LongestIncreasingSubsequenceLength(std::vector<int>& v)
-    {
-        if (v.size() == 0) // boundary case
-            return 0;
-
-        std::vector<int> tail(v.size(), 0);
-        int length = 1; // always points empty slot in tail
-
-        tail[0] = v[0];
-
-        for (int i = 1; i < v.size(); i++) {
-
-            // Do binary search for the element in
-            // the range from begin to begin + length
-            auto b = tail.begin(), e = tail.begin() + length;
-            auto it = lower_bound(b, e, v[i]);
-
-            // If not present change the tail element to v[i]
-            if (it == tail.begin() + length)
-                tail[length++] = v[i];
-            else
-                *it = v[i];
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> res;
+        for(int i=0; i<nums.size(); i++) {
+            auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+            if(it==res.end()) res.push_back(nums[i]);
+            else *it = nums[i];
         }
-
-        return length;
+        return res.size();
     }
     int maxEnvelopes(vector<vector<int>>& e) {
         vector<int> x;
         sort(e.begin(),e.end(),cmp);
         for(int i=0;i<e.size();i++)x.push_back(e[i][1]);
-        return LongestIncreasingSubsequenceLength(x);
+        return lengthOfLIS(x);
     }
 };

7943addf33790ae62c2b7be0bb3ff4ce27474445
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-06-09 12:45:10 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -17,7 +17,7 @@ public:
             ma=max(ma,matrix[i][c-1]);
         }
         
-        while(mi<ma){
+        while(mi<=ma){
             int mid=(mi+ma)/2;
             int cnt=0;
             for(int i=0;i<r;i++){
@@ -27,7 +27,7 @@ public:
                 mi=mid+1;
             }
             else{
-                ma=mid;
+                ma=mid-1;
             }
         }
         return mi;

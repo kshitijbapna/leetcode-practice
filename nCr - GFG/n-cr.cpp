62b76cb7134d4bedabd93c0b3aa52012038b6a4f
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-06-10 12:31:46 +0530

    Added solution - LeetHub

--- nCr - GFG/n-cr.cpp
+++ nCr - GFG/n-cr.cpp
@@ -9,28 +9,18 @@ using namespace std;
 
 class Solution{
 public:
-    int ncr[1001][801];
+    int ncr[1001][1001];
     int nCr(int n, int r){
-        //n!/r!(n-r)!
-        //ncr-1=r/(n-r+1)*ncr
         int m=(1e9+7);
         for(int i=1;i<1001;i++){
-            ncr[i][0]=1;
-            ncr[i][1]=i;
-            if(i<=800)
-            ncr[i][i]=1;
+            ncr[i][0]=1;ncr[i][1]=i;ncr[i][i]=1;
         }
         for(int i=3;i<1001;i++){
-            for(int j=1;j<=min(i,800);j++){
+            for(int j=1;j<=i;j++){
                 ncr[i][j]=(ncr[i-1][j]+ncr[i-1][j-1])%m;
+                if(i==n&&j==r)return ncr[n][r];
             }
         }
-        // for(int i=0;i<10;i++){
-        //     for(int j=0;j<10;j++){
-        //         cout<<ncr[i][j]<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
         return ncr[n][r];
     }
 };

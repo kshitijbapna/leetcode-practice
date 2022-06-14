75837cd7dcb2d9a18b0cffd9fd95f8a6cf580dad
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-06-14 11:32:14 +0530

    Time: 46 ms (25.24%), Space: 12.4 MB (17.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int longestSubse(string w1,string w2,int i,int j,vector<vector<int>>& dp){
+    int longestSubse(string &w1,string &w2,int i,int j,vector<vector<int>>& dp){
         if(i<0||j<0)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
         if(w1[i]==w2[j])return dp[i][j] = 1+longestSubse(w1,w2,i-1,j-1,dp);

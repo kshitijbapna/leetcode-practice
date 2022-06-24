275e98b0d05a4db75df43f39acccad985a25fe3e
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-06-24 20:50:15 +0530

    Time: 312 ms (63.44%), Space: 58.3 MB (39.03%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& ps) {
-        //In the Prim's algorithm, we are building a tree starting from some initial point. We track all connected points in visited. For the         //current point, we add its edges to the min heap. Then, we pick a smallest edge that connects to a point that is not visited. Repeat         //till all points are visited.
-        int n = ps.size(), res = 0, i = 0, connected = 0;
-        vector<bool> visited(n);
-        priority_queue<pair<int, int>> pq;
-        while (++connected < n) {
-            visited[i] = true;
-            for (int j = 0; j < n; ++j)
-                if (!visited[j])
-                    pq.push({-(abs(ps[i][0] - ps[j][0]) + abs(ps[i][1] - ps[j][1])), j});
-            while (visited[pq.top().second])
-                pq.pop();
-            res -= pq.top().first;
-            i = pq.top().second;
-            pq.pop();
+    int find_set(vector<int> &parent,int n){
+        if(parent[n]==-1)return n;
+        return parent[n]=find_set(parent,parent[n]);
+    }
+    bool unite(vector<int> &parent,vector<int> &rank,int x,int y){
+        int s1=find_set(parent,x);
+        int s2=find_set(parent,y);
+        if(s1==s2)return 0;
+        if(rank[s1]<rank[s2]){
+            parent[s1]=s2;
+            rank[s2]+=rank[s1];
         }
-        return res;
+        else{
+            parent[s2]=s1;
+            rank[s1]+=rank[s2];
+        }
+        return 1;
+    }
+    int minCostConnectPoints(vector<vector<int>>& ps) {
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        for(int i=0;i<ps.size();i++){
+            for(int j=i+1;j<ps.size();j++){
+                int dis=abs(ps[i][0]-ps[j][0])+abs(ps[i][1]-ps[j][1]);
+                pq.push({dis,{i,j}});
+            }
+        }
+        vector<int> parent(ps.size(),-1),rank(ps.size(),1);
+        int ans=0,k=ps.size()-1;
+        while(k>0){
+            pair<int,pair<int,int>> x=pq.top();
+            pq.pop();
+            if(unite(parent,rank,x.second.first,x.second.second)){
+                ans+=x.first;
+                k--;
+            }
+        }
+        return ans;
     }
 };

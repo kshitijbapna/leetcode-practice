1805365980c8a7381ff6f77c80d9c90a5b6185b0
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-06-27 12:45:45 +0530

    Time: 258 ms (16.31%), Space: 12.6 MB (62.13%) - LeetHub

---
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        //(y-y1)=m*(x-x1)
+        //y=m*x-m*x1+y1
+        set<pair<double,double>> s;
+        sort(points.begin(),points.end());
+        int n=points.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(points[i][0]==points[j][0])continue;
+                double num=points[i][1]-points[j][1];
+                double den=points[i][0]-points[j][0];
+                double m=num/den;
+                double c=points[i][1]-m*points[i][0];
+                s.insert({m,c});
+            }
+        }
+        map<int,int> m;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            m[points[i][0]]++;
+            ans=max(ans,m[points[i][0]]);
+        }
+        for(auto x : s){
+            double m=x.first,c=x.second;
+            int cnt=0;
+            for(int i=0;i<n;i++){
+                double rhs=m*points[i][0]+c;
+                double lhs=points[i][1];
+                if(abs(lhs-rhs)<=0.00001)cnt++;
+            }
+            ans=max(ans,cnt);
+        }
+        return ans;
+    }
+};

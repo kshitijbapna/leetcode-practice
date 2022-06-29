805cd65ba0e989d87ad2608e8602900464524b55
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-06-29 14:20:13 +0530

    Added solution - LeetHub

--- Smallest sum contiguous subarray - GFG/smallest-sum-contiguous-subarray.cpp
+++ Smallest sum contiguous subarray - GFG/smallest-sum-contiguous-subarray.cpp
@@ -11,11 +11,11 @@ using namespace std;
 class Solution{
   public:
   int smallestSumSubarray(vector<int>& a){
-      int n=a.size(),gmin=INT_MAX,curr_min=0,f=0;
+      int n=a.size(),gmin=INT_MAX,curr_min=0;
       for(int i=0;i<n;i++){
           curr_min+=a[i];
           gmin=min(gmin,curr_min);
-          if(curr_min>=0)curr_min=0;
+          if(curr_min>0)curr_min=0;
       }
       return gmin;
   }

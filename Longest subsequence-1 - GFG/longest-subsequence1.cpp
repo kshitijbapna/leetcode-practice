86f529cd8bdc59f11ba34dad0c69ef3c76b0ed0e
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-07-03 12:06:58 +0530

    Added solution - LeetHub

---
+++ Longest subsequence-1 - GFG/longest-subsequence1.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int longestSubsequence(int N, int A[])
+    {
+        vector<int> dp(N,1);
+        for(int i=1;i<N;i++){
+            for(int j=0;j<i;j++){
+                if(abs(A[i]-A[j])==1)dp[i]=max(dp[i],dp[j]+1);
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i = 0;i < N;i++)
+            cin>>A[i];
+        
+        Solution ob;
+        cout<<ob.longestSubsequence(N, A)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends

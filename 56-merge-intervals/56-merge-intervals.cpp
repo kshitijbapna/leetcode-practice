237ae4f9b1800a08d86720ee883d962696158e23
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-07-05 21:58:52 +0530

    Time: 54 ms (58.06%), Space: 19 MB (85.06%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -3,19 +3,15 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& in) {
         vector<vector<int>> ans;
         sort(in.begin(),in.end());
-        bool done[in.size()];
-        memset(done,0,sizeof(done));
-        for(int i=0;i<in.size();i++){
-            if(done[i]==0){
-                int st=in[i][0],en=in[i][1],j=i+1;
-                while(j<in.size()&&en>=in[j][0]){
-                    en=max(en,in[j][1]);
-                    done[j]=1;
-                    j++;
-                }
-                i=j-1;
-                ans.push_back({st,en});
+        int i=0,n=in.size();
+        while(i<n){
+            int st=in[i][0],en=in[i][1];
+            while(i+1<n&&en>=in[i+1][0]){
+                en=max(en,in[i+1][1]);
+                i++;
             }
+            ans.push_back({st,en});
+            i++;
         }
         return ans;
     }

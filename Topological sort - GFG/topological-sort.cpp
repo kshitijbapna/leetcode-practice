b0c4364bcfa5f78fe1ec22f067db53c459d1095b
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-07-06 20:47:05 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,33 +7,67 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	void tsort(int node,vector<int> &ans,vector<int> adj[],vector<bool> &vis){
-	    vis[node]=1;
-	    for(auto n : adj[node]){
-	        if(!vis[n]){
-	            tsort(n,ans,adj,vis);
-	        }
-	    }
-	    ans.push_back(node);
-	}
+	
+	/*Using dfs*/
+	
+// 	void tsort(int node,vector<int> &ans,vector<int> adj[],vector<bool> &vis){
+// 	    vis[node]=1;
+// 	    for(auto n : adj[node]){
+// 	        if(!vis[n]){
+// 	            tsort(n,ans,adj,vis);
+// 	        }
+// 	    }
+// 	    ans.push_back(node);
+// 	}
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	   vector<int> ans;
+// 	    vector<bool> vis(V);
+// 	    for(int i=0;i<V;i++){
+// 	        if(!vis[i]){
+// 	            tsort(i,ans,adj,vis);
+// 	        }
+// 	    }
+// 	    reverse(ans.begin(),ans.end());
+// 	    return ans;
+// 	}
+
+
+/*Using bfs*/
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	   // stack<int> s;
-	   vector<int> ans;
-	    vector<bool> vis(V);
+	    vector<int> ans;
+	    vector<int> vis(V),in(V);
 	    for(int i=0;i<V;i++){
-	        if(!vis[i]){
-	            tsort(i,ans,adj,vis);
+	        for(auto x : adj[i]){
+	            in[x]++;
 	        }
 	    }
-	   // vector<int> ans;
-	   // while(!s.empty()){
-	   //     ans.push_back(s.top());
-	   //     s.pop();
-	   // }
-	   reverse(ans.begin(),ans.end());
+	    queue<int> q;
+	    for(int i=0;i<V;i++){
+	        if(in[i]==0){
+	            q.push(i);
+	            vis[i]=1;
+	        }
+	    }
+	    while(q.size()>0){
+	        int x=q.front();
+	        q.pop();
+	        ans.push_back(x);
+	        for(auto neigh : adj[x]){
+	            if(!vis[neigh]){
+	                in[neigh]--;
+	                if(in[neigh]==0){
+	                    q.push(neigh);
+	                    vis[neigh]=1;
+	                }
+	            }
+	        }
+	    }
+	   // for(int i=0;i<ans.size();i++)cout<<ans[i]<<" ";
 	    return ans;
 	}
+    
 };
 
 // { Driver Code Starts.

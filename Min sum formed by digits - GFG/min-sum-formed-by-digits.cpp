143738c313e568e933f036104b3644fa5c0ec34c
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-07-15 00:05:08 +0530

    Added solution - LeetHub

---
+++ Min sum formed by digits - GFG/min-sum-formed-by-digits.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    long long int minSum(int arr[], int n)
+    {
+        string a="0",b="0";
+        sort(arr,arr+n);
+        for(int i=0;i<n;i++){
+            char x='0'+arr[i];
+            if(i%2)
+            a+=x;
+            else
+            b+=x;
+        }
+        return stoll(a)+stoll(b);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n+1];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.minSum(arr, n)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends

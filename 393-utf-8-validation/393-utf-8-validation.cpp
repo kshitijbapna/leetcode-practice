6283d77a348e235e360ab44f367b630b0eed539d
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-09-14 00:31:07 +0530

    Time: 26 ms (31.17%), Space: 14 MB (76.31%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int cnt=0;
+        for(auto x : data){
+            if(x>>3==0b11110)cnt+=3;
+            else if(x>>4==0b1110)cnt+=2;
+            else if(x>>5==0b110)cnt++;
+            else if(x>>6==0b10)cnt--;
+            else{
+                if(cnt>3||cnt<0)return 0;
+                if(x>>7==0b1)return 0;
+            }
+        }
+        return cnt==0;
+    }
+};

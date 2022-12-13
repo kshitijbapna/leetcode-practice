ee0957c1f97aa98764494f8506b60e83bf2edd50
kshitijbapna <57071713+kshitijbapna@users.noreply.github.com>
2022-12-13 14:04:07 +0530

    Time: 30 ms (60.11%), Space: 9.7 MB (95.85%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& a) {
+        int n=a.size(),m=a[0].size();
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(j==0){
+                    if(m>1)
+                    a[i][j]+=min(a[i-1][j],a[i-1][j+1]);
+                }
+                else if(j==m-1){
+                    if(m>1)
+                    a[i][j]+=min(a[i-1][j],a[i-1][j-1]);
+                }
+                else{
+                    a[i][j]+=min({a[i-1][j-1],a[i-1][j],a[i-1][j+1]});
+                }
+            }
+        }
+        return *min_element(a[n-1].begin(),a[n-1].end());
+    }
+};
